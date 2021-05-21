996b71e2d90ef9b7397d23376eb0349a86a88f70
Yerkanat <yerkanat.makhayev@gmail.com>
2021-05-21 23:49:42 +0600

    image uploaded

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useHistory} from 'react-router-dom'
 import {Container, Form, Button} from 'react-bootstrap'
 import './login.css'
@@ -13,11 +13,12 @@ const Login = (props) => {
         }
         
     }, [])
-    
+    const [imga, setimg] = useState(null)
 
     let submit = () => {
 
         var data = {
+            img: imga,
             guestName: document.getElementById('guestName').value,
             eventName: document.getElementById('eventName').value,
             guestInfo: document.getElementById('guestInfo').value,
@@ -31,24 +32,43 @@ const Login = (props) => {
                 'Content-Type':'application/json'
             }
         }
-        fetch('http://localhost/commitpost', options)
+        console.log(data)
+        fetch('http://localhost/giveimg', options)
         .then(res => res.json())
         .then(res => console.log(res))
     }
     let a = props.isAuth
+
+    
+    let fileHandler = e => {
+        let reader = new FileReader()
+        reader.readAsDataURL(e.target.files[0])
+
+        reader.onload = () => {
+            setimg(reader.result)
+            console.log('setimg')
+            // console.log(imga)
+        }
+        
+    }
+    
     return (
         <Container>
             <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label className = 'text'>Name Surname of the guest</Form.Label>
+                    <input type='file' onChange = {fileHandler}></input>
+                </Form.Group>
                 <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label class = 'text'>Name Surname of the guest</Form.Label>
+                    <Form.Label className = 'text'>Name Surname of the guest</Form.Label>
                     <Form.Control id = 'guestName' type= "text" placeholder="NameSurname of the guest" />
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlInput2">
-                    <Form.Label class = 'text'>The name of the event</Form.Label>
+                    <Form.Label className = 'text'>The name of the event</Form.Label>
                     <Form.Control id = 'eventName' type="text" placeholder="EventName" />
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlInput3">
-                    <Form.Label class = 'text'>About the guest</Form.Label>
+                    <Form.Label className = 'text'>About the guest</Form.Label>
                     <Form.Control  id = 'guestInfo' as="textarea" rows = {2} placeholder="Degrees, Jobs, Etc" />
                 </Form.Group>
                 
